9b8bff2f0243fd23a90876bd7831b99256779514
Jason <99057964@mydavinci.nl>
2021-11-16 11:01:26 +0100

    De tafel lab af.

---
+++ tafelmanieren /tafels.py
@@ -0,0 +1,26 @@
+
+#De eerste opdracht van de tafel lab.
+tafelInput = int(input('Type een getal in:'))
+
+if tafelInput > 0:
+    for i in range (1,11):
+        print(i * tafelInput)
+else:
+    print('Ongeldige invoer')
+
+print('\n')
+
+#De tweede opdracht van de tafel lab.
+for a in range (30,0,-1):
+    print(a)
+
+#De derde opdracht van de tafel lab.
+for x in range (1,24):
+    if x <= 12:
+        print(int(x) ,'am')
+    else:
+        print(int(x),'pm')
+
+#De vierde opdracht van de tafel lab.
+for b in range(20,51):
+    print(b)
