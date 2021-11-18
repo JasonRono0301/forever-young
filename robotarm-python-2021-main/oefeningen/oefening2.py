01c210d3ce691a14afee0a95ef72951a633ee5e3
Jason <99057964@mydavinci.nl>
2021-11-18 11:35:07 +0100

    Oefening 2 af.

--- robotarm-python-2021-main/oefeningen/oefening2.py
+++ robotarm-python-2021-main/oefeningen/oefening2.py
@@ -3,10 +3,21 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 2')
 
 # Jouw python instructies zet je vanaf hier:
-for i in range(1,0):
+for i in range(2):
     robotArm.grab()
-    for a in range(1,10):
+    for a in range(9):
         robotArm.moveRight()
+    robotArm.drop()
+    for b in range(5,0,-1):
+        robotArm.moveLeft()
+    robotArm.grab()
+    for c in range(3):
+        robotArm.moveRight()
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.moveRight()
+    robotArm.drop()
+
 
 
 # Na jouw code wachten tot het sluiten van de window:
